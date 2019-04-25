b5da8d7f48bc9b8b4ac371dc351164390d5eb1b6
qfrost <styopa1020@gmail.com>
2019-04-25 14:49:11 +0300

    change guard

--- src/app/guards/app.guard.ts
+++ src/app/guards/app.guard.ts
@@ -4,14 +4,17 @@ import { Observable, of } from 'rxjs';
 
 import * as fromApp from '../core/store/index';
 import { Store, select } from '@ngrx/store';
-import { map, take, switchMap } from 'rxjs/operators';
+import { map, take, switchMap, filter, catchError, tap } from 'rxjs/operators';
+import { loadUsers, selectUser, loadUsersSuccess } from '../core/store/app.action';
+import { AppService } from '../core/services/app.service';
+import { AppModel } from '../core/models/app.model';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AppGuard implements CanActivate {
 
-    constructor(private store: Store<fromApp.AppState>, private router: Router) {}
+    constructor(private store: Store<fromApp.AppState>, private appService: AppService, private router: Router) {}
 
     waitForUsersToLoad(): Observable<boolean> {
         return this.store.pipe(
@@ -21,17 +24,45 @@ export class AppGuard implements CanActivate {
         );
     }
 
+    hasSelectedUserInStore(id: string): Observable<boolean> {
+        return this.store.pipe(
+          select(fromApp.getUserById),
+          map(user => !!user),
+          take(1)
+        );
+    }
+
+    hasUsersInApi(id: string): Observable<boolean> {
+        return this.appService.getUsers().pipe(
+            map((users: AppModel.IUser[]) => loadUsersSuccess({users})),
+            tap(action => this.store.dispatch(action)),
+            map((users) => {
+                this.store.dispatch(selectUser({id: parseInt(id, 10)}));
+                return !!users;
+            }),
+            catchError(() => {
+                this.router.navigate(['/']);
+                return of(false);
+            })
+        );
+    }
+
+    hasUser(id) {
+        return this.hasSelectedUserInStore(id).pipe(
+            switchMap(inStore => {
+                if (inStore) {
+                    return of(inStore);
+                }
+
+                return this.hasUsersInApi(id);
+            })
+        );
+    }
+
 
     canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
         return this.waitForUsersToLoad().pipe(
-            switchMap((isLoaded) => {
-                if (!isLoaded) {
-                    this.router.navigate(['/']);
-                    return of(false);
-                }
-
-                return of(true);
-            })
+            switchMap(() => this.hasUser(route.params.id) )
         );
     }
 }
